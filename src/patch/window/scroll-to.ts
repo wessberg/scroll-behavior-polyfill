7597bba9067ac8ac26c38d003915e8be33e72c14
gretzkiy <dias7707@gmail.com>
2020-05-10 12:40:12 +0300

    fix: calling scroll methods without window context

--- src/patch/window/scroll-to.ts
+++ src/patch/window/scroll-to.ts
@@ -4,7 +4,7 @@ import {handleScrollMethod} from "../shared";
  * Patches the 'scrollTo' method on the Window prototype
  */
 export function patchWindowScrollTo(): void {
-	window.scrollTo = function(this: Window, optionsOrX?: number | ScrollToOptions, y?: number): void {
-		handleScrollMethod(this, "scrollTo", optionsOrX, y);
+	window.scrollTo = function(this: Window | undefined, optionsOrX?: number | ScrollToOptions, y?: number): void {
+		handleScrollMethod(this ?? window, "scrollTo", optionsOrX, y);
 	};
 }
