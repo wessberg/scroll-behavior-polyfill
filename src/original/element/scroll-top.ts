70914e56ed507bf3d8de6ed2505b46e2c2e4431d
Raphael Fischer <rfischer@OMNICOM.LOCAL>
2021-02-10 10:56:25 +0100

    fix: #15

--- src/original/element/scroll-top.ts
+++ src/original/element/scroll-top.ts
@@ -1,6 +1,4 @@
 import {UNSUPPORTED_ENVIRONMENT} from "../../support/unsupported-environment";
 
 const s = Object.getOwnPropertyDescriptor(Element.prototype, "scrollTop");
-export const ELEMENT_ORIGINAL_SCROLL_TOP_SET_DESCRIPTOR = (UNSUPPORTED_ENVIRONMENT || typeof s !== "undefined")
-	? undefined
-	: s!.set!;
+export const ELEMENT_ORIGINAL_SCROLL_TOP_SET_DESCRIPTOR = UNSUPPORTED_ENVIRONMENT || typeof s === "undefined" ? undefined : s!.set!;
